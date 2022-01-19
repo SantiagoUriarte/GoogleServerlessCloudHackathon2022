0c05943a4e6e35ce5fab36b4e648e1492d3e3f05
Santiago Uriarte <c.santiagou@gmail.com>
2022-01-19 06:23:57 -0800

    Added neccessary middleware: cors, morgan, helmet

--- backend/santiservice/index.js
+++ backend/santiservice/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(cors()); //Enable cors
+app.use(morgan()); //Generate more user logs
+app.use(helmet()); //Increase api security
 
 app.get("/about", (req, res) => {
   res.send("This is the santi microservice");
